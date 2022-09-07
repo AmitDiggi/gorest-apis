9cd62778ebbba01f3a4072fad603c8cec28afabb
Amit Kumar Diggi <amitkumardiggi05@gmail.com>
2022-09-07 17:35:50 +0530

    First commit

---
+++ src/test/java/SampleTests.java
@@ -0,0 +1,10 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SampleTests {
+
+    @Test
+    public void sampleTest(){
+        Assert.assertEquals(1,1);
+    }
+}
