8174946121a0a6555eb25438d5a5996bbe529b75
Amit Kumar Diggi <amitkumardiggi05@gmail.com>
2022-09-07 21:50:46 +0530

    initial commit
    sample rest api test

--- src/test/java/SampleTests.java
+++ src/test/java/SampleTests.java
@@ -1,10 +1,19 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import static io.restassured.RestAssured.given;
+
 public class SampleTests {
 
     @Test
     public void sampleTest(){
-        Assert.assertEquals(1,1);
+
+        given()
+                .when()
+                .get("https://gorest.co.in/public/v2/users")
+                .then()
+                .statusCode(200)
+                .log().body();
+       // Assert.assertEquals(1,1);
     }
 }
